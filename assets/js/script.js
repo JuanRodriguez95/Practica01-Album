8723921291ccb638cd202320bae55d39a76b9aa7
JuanRodriguez95 <juanrodriguezcampos818@gmail.com>
2022-01-21 10:59:24 -0500

    Commit del ejercicio 01 del semillero

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * 1. tema del ejercicio
+ * 2. crear carpeta assets/css - img
+ * 3. crear index y album
+ * 4. aplicar etiquetas ( title,img,p)
+ * 5. Titulo y tabla de 6*4 con imagenes referentes al   tema
+ * 
+ */
+
+
+// alert("Hola mundo!!");
+
+// var nombre = prompt("escriba su nombre");
+// var apellido = prompt("escriba su apellido");
+
+// var eleccion=confirm("seguro de continuar");
+// console.log(eleccion);
+// if(eleccion==true){
+//     alert("aqui esta la pagina");
+// }else{
+//     alert("aqui esta la pagina pero me dijo que no");
+// }
+// document.write(" su nombre es "+nombre +" "+ apellido);
